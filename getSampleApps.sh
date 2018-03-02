d0a69bd962af6e0f349e7aed98362c7bb1e7d59c
awjh-ibm <andrew.hurt1@ibm.com>
2018-03-02 16:22:27 +0000

    tempororary fix until samples published
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

---
+++ getSampleApps.sh
@@ -0,0 +1,6 @@
+rm -rf composer-sample-applications
+git clone https://github.com/hyperledger/composer-sample-applications
+cd composer-sample-applications
+npm install
+cd packages/vehicle-manufacture-car-builder
+npm run build
