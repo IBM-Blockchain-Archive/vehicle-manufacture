6bcf6874e7d8c2d1f8d0a92c7a422de958ab632b
Caroline Church <caroline.church@uk.ibm.com>
2018-03-07 13:53:06 +0000

    add service instance

--- .bluemix/pipeline.sh
+++ .bluemix/pipeline.sh
@@ -5,7 +5,7 @@ export IBP_NAME="ibm-blockchain-5-dev"
 export IBP_PLAN="ibm-blockchain-plan-v1-starter-dev"
 export VCAP_KEY_NAME="Credentials-1"
 export APP_URL="unknown_yet"  # we correct this later
-export SERVICE_INSTANCE="Blockchain-vehiclemanufacture-20180307105541823"
+export SERVICE_INSTANCE_NAME="Blockchain-vehiclemanufacture-20180307105541823"
 
 detect_exit() {
     if [ "$DEPLOY_STATUS" != "sample_up" ]; then
