4428e8554fde2c25a91fd84b306481b29894c57e
Caroline Church <caroline.church@uk.ibm.com>
2018-03-08 09:43:40 +0000

    add cloudant wallet to playground

--- .bluemix/pipeline.sh
+++ .bluemix/pipeline.sh
@@ -286,17 +286,17 @@ EOF
   printf "\n ---- Install composer-playground ----- \n"
   npm install composer-playground@next
 
-  npm -g install git+https://github.com/ampretia/composer-wallet-cloudant.git
+  npm -g install @ampretia/composer-wallet-cloudant
 
   read -d '' NODE_CONFIG << EOF
-{"composer":{"wallet":{"type":"@ampretia/composer-wallet-ibmcos","desc":"Uses cloud wallet","options":${CLOUDANT_CREDS}}}}
+{"composer":{"wallet":{"type":"@ampretia/composer-wallet-cloudant","desc":"Uses cloud wallet","options":${CLOUDANT_CREDS}}}}
 EOF
   export NODE_CONFIG
 
   composer card import -f ./admin@vehicle-manufacture-network.card
 
   cd node_modules/composer-playground
-  npm install git+https://github.com/ampretia/composer-wallet-cloudant.git
+  npm install @ampretia/composer-wallet-cloudant
 
   cf push composer-playground-${CF_APP} -c "node cli.js" -i 1 -m 128M --no-start
   cf set-env composer-playground-${CF_APP} NODE_CONFIG "${NODE_CONFIG}"
