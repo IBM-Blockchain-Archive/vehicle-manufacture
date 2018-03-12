a3dce5d00c19efeb547eefaca656f487ec31e12b
Caroline Church <caroline.church@uk.ibm.com>
2018-03-12 13:30:48 +0000

    run end to end

--- .bluemix/pipeline.sh
+++ .bluemix/pipeline.sh
@@ -282,14 +282,11 @@ EOF
   export DEPLOY_STATUS="installed_cc"
   update_status
 
-  #Make sure peer is up
-  sleep 60s
-
   printf "\n --- start network --- \n"
 
   while ! composer network start -c adminCard -a vehicle-manufacture-network.bna -A admin -C ./credentials/admin-pub.pem -f delete_me.card; do
     echo sleeping to retry network start
-    sleep 60s
+    sleep 30s
   done
 
   composer card create -n vehicle-manufacture-network -p ./config/connection-profile.json -u admin -c ./credentials/admin-pub.pem -k ./credentials/admin-priv.pem
